6ef4e8c99969cdac9117d554fcda7e425d7fdc08
agim <mynameisagim@gmail.com>
2021-11-17 21:15:00 +0700

    susu kamu

--- src/testtugasakhir/TestTugasAkhir.java
+++ src/testtugasakhir/TestTugasAkhir.java
@@ -15,6 +15,7 @@ public class TestTugasAkhir {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println(" oki kun........");
     }
-    
+  
 }
